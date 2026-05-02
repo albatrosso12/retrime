6f5ef105c45d733c9853cf2763b76f85cacf8006
albatrosso12 <korsetov2009@gmail.com>
2026-05-02 06:26:03 +0300

    fix

--- cloudflare-zapier-proxy/src/index.ts
+++ cloudflare-zapier-proxy/src/index.ts
@@ -14,7 +14,7 @@ function corsHeaders() {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   };
 }
 
